82a8330eb3edc94ef35f06716c7f1957f1089f4c
KemalBekir <outsider.pld@gmail.com>
2022-03-20 22:01:48 +0000

    Added user profile to initial backend skeleton

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const cors = require('./middleware/cors');
+const catalogController = require('./controllers/catalog');
+const usersController = require('./controllers/users');
+const auth = require('./middleware/auth');
 
 start();
 
-async function start(){
+async function start() {
     //TODO: Give DB appropriate name for the project
     const dbName = 'data';
     //TODO: Change PORT Number
@@ -16,17 +19,24 @@ async function start(){
             useNewUrlParser: true
         });
         console.log('Database ready');
-    } catch (error) {   
+    } catch (error) {
         console.error('Database connection failed');
         process.exit(1);
     }
-    
+
     const app = express();
     app.use(express.json());
     app.use(cors());
+    app.use(auth());
+    app.use('/data/catalog', catalogController);
+    app.use('/users', usersController);
 
-    app.get('/', (req,res) => {
-        res.json({message: 'REST servise operational'});
+
+    //TODO: Change path name according to the requirements of the project
+    app.use('/data/catalog', catalogController);
+
+    app.get('/', (req, res) => {
+        res.json({ message: 'REST servise operational' });
     });
 
     app.listen(PORT, () => console.log(`REST service started on port ${PORT}`));
---
+++ backend/services/item.js
@@ -0,0 +1,47 @@
+const Item = require('../models/item');
+
+async function getAll(){
+    return Item.find({});
+}
+
+function getById(id){
+    return Item.findById(id);
+}
+
+async function create(item){
+    const result = new Item(item);
+    await result.save();
+
+    return result;
+}
+
+async function update(id, item){
+    const existing = await Item.findById(id);
+
+     //TODO: Change names according to requirements
+
+    existing.make = item.make;
+    existing.model = item.model;
+    existing.year = item.year;
+    existing.description = item.description;
+    existing.price = item.price;
+    existing.img = item.img;
+    existing.material = item.material;
+
+    await existing.save();
+
+    return existing;
+}
+
+async function deleteById(id){
+    await Item.findByIdAndDelete(id);
+}
+
+
+module.exports = {
+    create,
+    getAll,
+    getById,
+    update,
+    deleteById,
+}
